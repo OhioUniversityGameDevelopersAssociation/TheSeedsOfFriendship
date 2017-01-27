fb2d41c43804d0cec16f00c697ca60044b6a87c8
Daggett110 <andrewdecker1@live.com>
2017-01-26 22:02:49 -0500

    Second Backup
    Moving back to other computer. Making backup

--- Fists of Friendship/Assets/Scripts/Misc/MainMenu.cs
+++ Fists of Friendship/Assets/Scripts/Misc/MainMenu.cs
@@ -1,20 +1,33 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
 
     public EventSystem eventSystem;
     public GameObject happyAnimals;
     public string startSceneName;
-    public GameObject credits;
+    public GameObject mainMenu;
     public GameObject creditsButton, backButton;
+
+    public Image screenFade;
+    public SpriteRenderer[] fruitCakeSprites, watermelonSprites;
+    private bool fadeToBlack;
+    private bool currentlyOnFruitcake;
 
     private void Start()
     {
-        if(PlayerPrefs.GetInt("BeatenOnce") == 1)
+        if (PlayerPrefs.GetInt("BeatenOnce") == 1)
         {
             happyAnimals.SetActive(true);
+        }
+        if (PlayerPrefs.GetInt("JustBeaten") == 1)
+        {
+            Credits();
+            PlayerPrefs.SetInt("JustBeaten", 0);
         }
     }
 
@@ -25,18 +38,22 @@ public class MainMenu : MonoBehaviour {
 
     public void Credits()
     {
-        credits.SetActive(true);
+        mainMenu.SetActive(false);
         eventSystem.SetSelectedGameObject(backButton);
+        StartCoroutine(CreditsScrolling());
     }
 
     public void ExitCredits()
     {
-        credits.SetActive(false);
+        mainMenu.SetActive(true);
         eventSystem.SetSelectedGameObject(creditsButton);
+        StopAllCoroutines();
     }
 
     public void Quit()
     {
+        PlayerPrefs.SetInt("BeatenOnce", 0);
+        PlayerPrefs.SetInt("JustBeaten", 0);
         Application.Quit();
     }
 
@@ -44,5 +61,68 @@ public class MainMenu : MonoBehaviour {
     {
         source.volume = Random.Range(0.7f, 0.9f);
         source.pitch = Random.Range(0.8f, 1.2f);
+    }
+
+    // It would be nice to simply turn these off, but then our paralax/scrolling scripts wouldn't work.
+    // We'll need to disable the sprites, and have a black cover fade in between the switch
+    IEnumerator CreditsScrolling()
+    {
+        // Randomizing the background, so it doesn't always have the same one
+        if (Random.value >= 0.5)
+            currentlyOnFruitcake = true;
+        else
+            currentlyOnFruitcake = false;
+
+        // Turn the correct starting renderers on and off
+        foreach (SpriteRenderer rend in fruitCakeSprites)
+        {
+            rend.enabled = currentlyOnFruitcake;
+        }
+        foreach (SpriteRenderer rend in watermelonSprites)
+        {
+            rend.enabled = !currentlyOnFruitcake;
+        }
+
+
+        while (true)
+        {
+            if (mainMenu.activeSelf)
+            {
+                break;
+            }
+
+            yield return new WaitForSeconds(Random.Range(5f, 8f));
+
+            float t = 0;
+
+
+            while (t < 1)
+            {
+                t += Time.deltaTime;
+                screenFade.color = Color.Lerp(Color.clear, Color.black, t);
+                yield return null;
+            }
+
+            currentlyOnFruitcake = !currentlyOnFruitcake;
+
+            // Turn the correct starting renderers on and off
+            foreach (SpriteRenderer rend in fruitCakeSprites)
+            {
+                rend.enabled = currentlyOnFruitcake;
+            }
+            foreach (SpriteRenderer rend in watermelonSprites)
+            {
+                rend.enabled = !currentlyOnFruitcake;
+            }
+
+            yield return new WaitForSeconds(0.5f);
+
+            while (t > 0)
+            {
+                t -= Time.deltaTime;
+                screenFade.color = Color.Lerp(Color.clear, Color.black, t);
+                yield return null;
+            }
+        }
     }
 }
---
+++ Fists of Friendship/Assets/Scripts/Misc/MoveHorizontal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MoveHorizontal : MonoBehaviour
+{
+
+    public bool movingLeft;
+    public float speed;
+
+    int directionalMultiplier;
+    Vector3 newPosition;
+
+    void Awake()
+    {
+        directionalMultiplier = movingLeft ? -1 : 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPosition.Set(
+            transform.position.x + (speed * directionalMultiplier * Time.deltaTime),
+            transform.position.y,
+            transform.position.z);
+        transform.position = newPosition;
+    }
+}
